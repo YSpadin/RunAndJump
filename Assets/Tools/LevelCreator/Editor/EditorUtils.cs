979197691c081cd816bf9fabf270962a2e887cd5
YSpadin <62306659+YSpadin@users.noreply.github.com>
2021-03-22 23:51:07 +0100

    ASDF

---
+++ Assets/Tools/LevelCreator/Editor/EditorUtils.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+namespace RunAndJump.LevelCreator
+{
+    public static class EditorUtils
+    {
+
+        // Creates a new scene
+        public static void NewScene()
+        {
+            EditorApplication.SaveCurrentSceneIfUserWantsTo();
+            EditorApplication.NewScene();
+        }
+        // Remove all the elements of the scene
+        public static void CleanScene()
+        {
+            GameObject[] allObjects = Object.FindObjectsOfType<GameObject>
+           ();
+            foreach (GameObject go in allObjects)
+            {
+                GameObject.DestroyImmediate(go);
+            }
+        }
+        // Creates a new scene capable to be used as a level
+        public static void NewLevel()
+        {
+            NewScene();
+            CleanScene();
+            GameObject levelGO = new GameObject("Level");
+            levelGO.transform.position = Vector3.zero;
+            levelGO.AddComponent<Level>();
+        }
+    }
+}
---
+++ Assets/Tools/LevelCreator/Editor/MenuItems.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEditor;
+namespace RunAndJump.LevelCreator
+{
+    public static class MenuItems
+    {
+
+        [MenuItem("Tools/Level Creator/New Level Scene")]
+        private static void NewLevel()
+        {
+            EditorUtils.NewLevel();
+        }
+    }
+}
