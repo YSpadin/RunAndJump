f33d2829385eaa803d4dc1b451620334f97635bc
YSpadin <62306659+YSpadin@users.noreply.github.com>
2021-03-29 17:09:56 +0200

    adf

--- Assets/Tools/LevelCreator/Editor/LevelInspector.cs
+++ Assets/Tools/LevelCreator/Editor/LevelInspector.cs
@@ -1,16 +1,53 @@
 ﻿using UnityEngine;
 using UnityEditor;
+using System.Collections.Generic;
+
 namespace RunAndJump.LevelCreator
 {
     [CustomEditor(typeof(Level))]
     public class LevelInspector : Editor
     {
+        public enum Mode
+        {
+            View,
+            Paint,
+            Edit,
+            Erase,
+        }
+
+        private Mode _selectedMode;
+        private Mode _currentMode;
+
         private int _newTotalColumns;
         private int _newTotalRows;
         private Level _myTarget;
         private PaletteItem _itemSelected;
         private Texture2D _itemPreview;
         private LevelPiece _pieceSelected;
+        private PaletteItem _itemInspected;
+        private int _originalPosX;
+        private int _originalPosY;
+
+        private void Move()
+        {
+            Vector3 gridPoint = _myTarget.WorldToGridCoordinates(_itemInspected.transform.position);
+            int col = (int)gridPoint.x;
+            int row = (int)gridPoint.y;
+            if (col == _originalPosX && row == _originalPosY)
+            {
+                return;
+            }
+            if (!_myTarget.IsInsideGridBounds(col, row) || _myTarget.Pieces[col + row * _myTarget.TotalColumns] != null)
+            {
+                _itemInspected.transform.position = _myTarget.GridToWorldCoordinates(_originalPosX, _originalPosY);
+            }
+            else
+            {
+                _myTarget.Pieces[_originalPosX + _originalPosY * _myTarget.TotalColumns] = null;
+                _myTarget.Pieces[col + row * _myTarget.TotalColumns] = _itemInspected.GetComponent<LevelPiece>();
+                _myTarget.Pieces[col + row * _myTarget.TotalColumns].transform.position = _myTarget.GridToWorldCoordinates(col, row);
+            }
+        }
         private void OnEnable()
         {
             Debug.Log("OnEnable was called...");
@@ -35,17 +72,14 @@ namespace RunAndJump.LevelCreator
         }
         private void UnsubscribeEvents()
         {
-            PaletteWindow.ItemSelectedEvent -= new PaletteWindow.
-itemSelectedDelegate(UpdateCurrentPieceInstance);
+            PaletteWindow.ItemSelectedEvent -= new PaletteWindow.itemSelectedDelegate(UpdateCurrentPieceInstance);
         }
         private void DrawPieceSelectedGUI()
         {
-            EditorGUILayout.LabelField("Piece Selected", EditorStyles.
-            boldLabel);
+            EditorGUILayout.LabelField("Piece Selected", EditorStyles.boldLabel);
             if (_pieceSelected == null)
             {
-                EditorGUILayout.HelpBox("No piece selected!", MessageType.
-                Info);
+                EditorGUILayout.HelpBox("No piece selected!", MessageType.Info);
             }
             else
             {
@@ -75,6 +109,7 @@ itemSelectedDelegate(UpdateCurrentPieceInstance);
             DrawLevelDataGUI();
             DrawLevelSizeGUI();
             DrawPieceSelectedGUI();
+            DrawInspectedItemGUI();
             if (GUI.changed)
             {
                 EditorUtility.SetDirty(_myTarget);
@@ -85,23 +120,18 @@ itemSelectedDelegate(UpdateCurrentPieceInstance);
             EditorGUILayout.LabelField("Size", EditorStyles.boldLabel);
             EditorGUILayout.BeginHorizontal("box");
             EditorGUILayout.BeginVertical();
-            _newTotalColumns = EditorGUILayout.IntField("Columns", Mathf.Max
-(1, _newTotalColumns));
+            _newTotalColumns = EditorGUILayout.IntField("Columns", Mathf.Max(1, _newTotalColumns));
             _newTotalRows = EditorGUILayout.IntField("Rows", Mathf.Max(1, _newTotalRows));
             EditorGUILayout.EndVertical();
+
             EditorGUILayout.BeginVertical();
             // with this variable we can enable or disable GUI
             bool oldEnabled = GUI.enabled;
             GUI.enabled = (_newTotalColumns != _myTarget.TotalColumns || _newTotalRows != _myTarget.TotalRows);
-            bool buttonResize = GUILayout.Button("Resize", GUILayout.Height(2
-            * EditorGUIUtility.singleLineHeight));
+            bool buttonResize = GUILayout.Button("Resize", GUILayout.Height(2 * EditorGUIUtility.singleLineHeight));
             if (buttonResize)
             {
-                if (EditorUtility.DisplayDialog(
-                "Level Creator",
-                "Are you sure you want to resize the level?\nThis action cannote undone.",
-                "Yes",
-                "No"))
+                if (EditorUtility.DisplayDialog("Level Creator", "Are you sure you want to resize the level?\nThis action cannote undone.", "Yes", "No"))
                 {
                     ResizeLevel();
                 }
@@ -120,8 +150,8 @@ itemSelectedDelegate(UpdateCurrentPieceInstance);
             if (_myTarget.Pieces == null || _myTarget.Pieces.Length == 0)
             {
                 Debug.Log("Initializing the Pieces array...");
-                _myTarget.Pieces = new LevelPiece[_myTarget.TotalColumns *
-                _myTarget.TotalRows];
+                _myTarget.Pieces = new LevelPiece[_myTarget.TotalColumns * _myTarget.TotalRows];
+                _myTarget.transform.hideFlags = HideFlags.NotEditable;
             }
         }
         private void ResetResizeValues()
@@ -138,9 +168,7 @@ itemSelectedDelegate(UpdateCurrentPieceInstance);
                 {
                     if (col < _newTotalColumns && row < _newTotalRows)
                     {
-                        newPieces[col + row * _newTotalColumns] =
-                        _myTarget.Pieces[col + row * _myTarget.
-                        TotalColumns];
+                        newPieces[col + row * _newTotalColumns] = _myTarget.Pieces[col + row * _myTarget.TotalColumns];
                     }
                     else
                     {
@@ -161,16 +189,182 @@ itemSelectedDelegate(UpdateCurrentPieceInstance);
         {
             EditorGUILayout.LabelField("Data", EditorStyles.boldLabel);
             EditorGUILayout.BeginVertical("box");
-            _myTarget.TotalTime = EditorGUILayout.IntField("Total Time", Mathf.
-            Max(0, _myTarget.TotalTime));
+            _myTarget.TotalTime = EditorGUILayout.IntField("Total Time", Mathf.Max(0, _myTarget.TotalTime));
 
-            _myTarget.Gravity = EditorGUILayout.FloatField("Gravity",
-            _myTarget.Gravity);
-            _myTarget.Bgm = (AudioClip)EditorGUILayout.ObjectField("Bgm",
-            _myTarget.Bgm, typeof(AudioClip), false);
-            _myTarget.Background = (Sprite)EditorGUILayout.ObjectField
-            ("Background", _myTarget.Background, typeof(Sprite), false);
+            _myTarget.Gravity = EditorGUILayout.FloatField("Gravity", _myTarget.Gravity);
+            _myTarget.Bgm = (AudioClip)EditorGUILayout.ObjectField("Bgm", _myTarget.Bgm, typeof(AudioClip), false);
+            _myTarget.Background = (Sprite)EditorGUILayout.ObjectField("Background", _myTarget.Background, typeof(Sprite), false);
             EditorGUILayout.EndVertical();
+        }
+
+        private void DrawModeGUI()
+        {
+            List<Mode> modes = EditorUtils.GetListFromEnum<Mode>();
+            List<string> modeLabels = new List<string>();
+            foreach (Mode mode in modes)
+            {
+                modeLabels.Add(mode.ToString());
+            }
+            Handles.BeginGUI();
+            GUILayout.BeginArea(new Rect(10f, 10f, 360, 40f));
+            _selectedMode = (Mode)GUILayout.Toolbar(
+            (int)_currentMode,
+            modeLabels.ToArray(),
+            GUILayout.ExpandHeight(true));
+            GUILayout.EndArea();
+            Handles.EndGUI();
+        }
+        private void OnSceneGUI()
+        {
+            DrawModeGUI();
+            ModeHandler();
+            EventHandler();
+        }
+        private void Paint(int col, int row)
+        {
+            // Check out of bounds and if we have a piece selected
+            if (!_myTarget.IsInsideGridBounds(col, row) || _pieceSelected == null)
+            {
+                return;
+            }
+            // Check if I need to destroy a previous piece
+            if (_myTarget.Pieces[col + row * _myTarget.TotalColumns] != null)
+            {
+                DestroyImmediate(_myTarget.Pieces[col + row * _myTarget.TotalColumns].gameObject);
+            }
+            // Do paint !
+            GameObject obj = PrefabUtility.InstantiatePrefab(_pieceSelected.gameObject) as GameObject;
+            obj.transform.parent = _myTarget.transform;
+            obj.name = string.Format("[{0},{1}][{2}]", col, row, obj.name);
+            obj.transform.position = _myTarget.GridToWorldCoordinates(col, row);
+            obj.hideFlags = HideFlags.HideInHierarchy;
+            _myTarget.Pieces[col + row * _myTarget.TotalColumns] = obj.GetComponent<LevelPiece>();
+        }
+        private void Erase(int col, int row)
+        {
+            // Check out of bounds
+            if (!_myTarget.IsInsideGridBounds(col, row))
+            {
+                return;
+            }
+            // Do Erase
+            if (_myTarget.Pieces[col + row * _myTarget.TotalColumns] != null)
+            {
+                DestroyImmediate(_myTarget.Pieces[col + row * _myTarget.TotalColumns].gameObject);
+            }
+        }
+
+        private void Edit(int col, int row)
+        {
+            // Check out of bounds
+            if (!_myTarget.IsInsideGridBounds(col, row) || _myTarget.Pieces[col + row * _myTarget.TotalColumns] == null)
+            {
+                _itemInspected = null;
+            }
+            else
+            {
+                _itemInspected = _myTarget.Pieces[col + row * _myTarget.TotalColumns].GetComponent<PaletteItem>() as PaletteItem;
+            }
+            Repaint();
+        }
+
+        private void EventHandler()
+        {
+            HandleUtility.AddDefaultControl(GUIUtility.GetControlID(FocusType.Passive));
+
+            Camera camera = SceneView.currentDrawingSceneView.camera;
+            Vector3 mousePosition = Event.current.mousePosition;
+            mousePosition = new Vector2(mousePosition.x, camera.pixelHeight - mousePosition.y);
+            //Debug.LogFormat("MousePos: {0}", mousePosition);
+            Vector3 worldPos = camera.ScreenToWorldPoint(mousePosition);
+            Vector3 gridPos = _myTarget.WorldToGridCoordinates(worldPos);
+            int col = (int)gridPos.x;
+            int row = (int)gridPos.y;
+            Debug.LogFormat("GridPos {0},{1}", col, row);
+            switch (_currentMode)
+            {
+                case Mode.Paint:
+                    if (Event.current.type == EventType.MouseDown || Event.current.type == EventType.MouseDrag)
+                    {
+                        Paint(col, row);
+                    }
+                    break;
+                case Mode.Edit:
+                    if (Event.current.type == EventType.MouseDown)
+                    {
+                        Edit(col, row);
+                        _originalPosX = col;
+                        _originalPosY = row;
+                    }
+                    if (Event.current.type == EventType.MouseUp || Event.current.type == EventType.Ignore)
+                    {
+                        if (_itemInspected != null)
+                        {
+                            Move();
+                        }
+                    }
+                    if (_itemInspected != null)
+                    {
+                        _itemInspected.transform.position = Handles.FreeMoveHandle(_itemInspected.transform.position, _itemInspected.transform.rotation, Level.GridSize / 2, Level.GridSize / 2 * Vector3.one, Handles.RectangleCap);
+                    }
+                    break;
+                case Mode.Erase:
+                    if (Event.current.type == EventType.MouseDown || Event.current.type == EventType.MouseDrag)
+                    {
+                        Erase(col, row);
+                    }
+                    break;
+                case Mode.View:
+                default:
+                    break;
+            }
+        }
+
+        private void ModeHandler()
+        {
+            switch (_selectedMode)
+            {
+                case Mode.Paint:
+                case Mode.Edit:
+                case Mode.Erase:
+                    Tools.current = Tool.None;
+                    break;
+                case Mode.View:
+                default:
+                    Tools.current = Tool.View;
+                    break;
+            }
+            // Detect Mode change
+            if (_selectedMode != _currentMode)
+            {
+                _currentMode = _selectedMode;
+                _itemInspected = null;
+                Repaint();
+            }
+            // Force 2D Mode!
+            SceneView.currentDrawingSceneView.in2DMode = true;
+        }
+        private void DrawInspectedItemGUI()
+        {
+            // Only show this GUI if we are in edit mode.
+            if (_currentMode != Mode.Edit)
+            {
+                return;
+            }
+            //EditorGUILayout.LabelField ("Piece Edited", _titleStyle);
+            EditorGUILayout.LabelField("Piece Edited", EditorStyles.boldLabel);
+            if (_itemInspected != null)
+            {
+                EditorGUILayout.BeginVertical("box");
+                EditorGUILayout.LabelField("Name: " + _itemInspected.name);
+                Editor.CreateEditor(
+                _itemInspected.inspectedScript).OnInspectorGUI();
+                EditorGUILayout.EndVertical();
+            }
+            else
+            {
+                EditorGUILayout.HelpBox("No piece to edit!", MessageType.Info);
+            }
         }
     }
 }
